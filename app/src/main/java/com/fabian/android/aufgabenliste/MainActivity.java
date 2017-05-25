df33a84add719ed4172a74c265304a0b6877382d
Fabian <fabiandollas@web.de>
2017-05-25 23:10:46 +0200

    Funktion ViewPagerAdapter geschrieben

--- app/src/main/java/com/fabian/android/aufgabenliste/MainActivity.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         setupViewPager (viewPager);
+    }
+
+    private void setupViewPager (ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter (getSupportFragmentManager());
+        adapter.addFragment (new OneFragment(), "Aufgaben");
+        viewPager.setAdapter(adapter);
     }
 }
