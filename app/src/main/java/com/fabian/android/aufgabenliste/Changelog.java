f0fa99ad009ada45069e4162086e9e2d10060263
Fabian <fabiandollas@web.de>
2017-05-27 14:14:22 +0200

    in Klasse Changelog Funktion onCreate erstellt

--- app/src/main/java/com/fabian/android/aufgabenliste/Changelog.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Changelog.java
@@ -1,8 +1,23 @@
 package com.fabian.android.aufgabenliste;
 
-/**
- * Created by root on 27.05.17.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class Changelog {
+public class Changelog extends AppCompatActivity
+{
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate (Bundle savedInstancesState)
+    {
+        super.onCreate (savedInstancesState);
+        setContentView (R.layout.activity_changelog);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarVersion);
+        setSupportActionBar (toolbar);
+        toolbar.setSubtitle (R.string.Untertitel);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled (true);
+    }
 }
