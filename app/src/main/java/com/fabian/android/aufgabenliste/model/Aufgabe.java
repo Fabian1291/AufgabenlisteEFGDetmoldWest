dddfc87306ae4982606b6c165f77d874e3d9f3a8
Fabian <fabiandollas@web.de>
2017-06-05 11:31:34 +0200

    in Klasse Aufgabe Variablen id, Aufgabe, Datum, Ort, Ersteller, Prioritaet, Beschreibung, Erledigt, Erldediger, DatumErledigt initialisiert

--- app/src/main/java/com/fabian/android/aufgabenliste/model/Aufgabe.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/model/Aufgabe.java
@@ -1,4 +1,17 @@
 package com.fabian.android.aufgabenliste.model;
 
-public class Aufgabe {
+import java.util.Calendar;
+
+public class Aufgabe
+{
+    private long id;
+    private String Aufgabe;
+    private Calendar Datum;
+    private String Ort;
+    private String Ersteller;
+    private int Prioritaet;
+    private String Beschreibung;
+    private int Erledigt;
+    private String Erlediger;
+    private Calendar DatumErledigt;
 }
