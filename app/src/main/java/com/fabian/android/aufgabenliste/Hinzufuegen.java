ae5726a4bdbd8480cea829427e2c1f645a71a100
Fabian <fabiandollas@web.de>
2017-06-07 10:59:13 +0200

    in Klasse Hinzufuegen Funktionen sendMessage und getUsername eingefügt

--- app/src/main/java/com/fabian/android/aufgabenliste/Hinzufuegen.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Hinzufuegen.java
@@ -1,8 +1,22 @@
 package com.fabian.android.aufgabenliste;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Hinzufuegen extends AppCompatActivity
 {
@@ -20,4 +34,65 @@ public class Hinzufuegen extends AppCompatActivity
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        EditText editText3 = (EditText) findViewById(R.id.editText3);
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        String Aufgabe = editText.getText().toString();
+        String Ort = editText2.getText().toString();
+        String Beschreibung= editText3.getText().toString();
+        String Prioritaet = String.valueOf(spinner1.getSelectedItem());
+
+        if (Aufgabe.length() == 0)
+        {
+            Toast.makeText(this,"Bitte das Feld 'Aufgabe' ausfüllen", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (Ort.length() == 0)
+        {
+            Toast.makeText(this,"Bitte das Feld 'Ort' ausfüllen", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date today = Calendar.getInstance().getTime();
+        String reportDate = df.format(today);
+
+        try {
+            dataSource.open();
+            dataSource.createEntry(Aufgabe, Ort, Beschreibung, getUsername(), null, 0, Prioritaet, reportDate, "0");
+            dataSource.close();
+        }
+        catch (Exception ex) {
+            Toast.makeText(this,ex.toString(), Toast.LENGTH_LONG).show();
+        }
+        startActivity(intent);
+    }
+    public String getUsername() {
+        AccountManager manager = AccountManager.get(this);
+        Account[] accounts = manager.getAccountsByType("com.google");
+        List<String> possibleEmails = new LinkedList<String>();
+
+        for (Account account : accounts) {
+            // TODO: Check possibleEmail against an email regex or treat
+            // account.name as an email address only for certain account.type
+            // values.
+            possibleEmails.add(account.name);
+        }
+
+        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
+            String email = possibleEmails.get(0);
+            String[] parts = email.split("@");
+            if (parts.length > 0 && parts[0] != null)
+                return parts[0];
+            else
+                return null;
+        } else
+            return null;
+    }
+
 }
