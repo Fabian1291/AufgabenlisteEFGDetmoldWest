2b6ae84edc0a6722c0dd1a684c6bcde81860de46
Fabian <fabiandollas@web.de>
2017-06-07 12:14:46 +0200

    in Klasse Hinzufuegen wird das Datum mit übergeben

--- app/src/main/java/com/fabian/android/aufgabenliste/Hinzufuegen.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Hinzufuegen.java
@@ -67,7 +67,7 @@ public class Hinzufuegen extends AppCompatActivity
 
         AufgabenlisteDatabase database = AufgabenlisteDatabase.getInstance(Hinzufuegen.this);
 
-        database.createAufgabe(new Aufgabe(Aufgabe));
+        database.createAufgabe(new Aufgabe(Aufgabe, reportDate));
 
         startActivity(intent);
     }
