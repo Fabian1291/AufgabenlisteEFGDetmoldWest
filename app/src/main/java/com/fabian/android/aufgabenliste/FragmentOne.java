9f82b2f477c5a84c04a386bbd8838764f1730b0d
Fabian <fabiandollas@web.de>
2017-06-07 10:21:19 +0200

    in Klasse FragmentOne Funktion refreshListView entfernt

--- app/src/main/java/com/fabian/android/aufgabenliste/FragmentOne.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/FragmentOne.java
@@ -1,5 +1,6 @@
 package com.fabian.android.aufgabenliste;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -29,22 +30,17 @@ public class FragmentOne extends Fragment
 
         this.adapter = new AufgabenlisteAdapter (this, AufgabenlisteDatabase.getInstance (this).getAllAufgabenAsCursor ());
         this.listView.setAdapter (adapter);
-        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        this.listView.setOnItemClickListener (new AdapterView.OnItemClickListener ()
         {
             @Override
-            public void onItemClick(final AdapterView<?> adapterView, final View view, final int i, final long l)
+            public void onItemClick (final AdapterView<?> adapterView, final View view, final int i, final long l)
             {
-                Object element = adapterView.getAdapter().getItem(i);
+                Object element = adapterView.getAdapter ().getItem (i);
 
                 Log.e("ClickOnList", element.toString());
 
             }
         });
-
-        private void refreshListView ()
-        {
-            adapter.changeCursor (AufgabenlisteDatabase.getInstance (this).getAllTodosAsCursor ());
-        }
 
         return view;
     }
