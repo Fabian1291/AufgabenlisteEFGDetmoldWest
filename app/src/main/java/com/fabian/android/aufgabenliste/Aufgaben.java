ca9bf4f2e5ce6937ce5d185cd367d15587c49b6f
Fabian <fabiandollas@web.de>
2017-06-13 12:05:11 +0200

    in Klasse Aufgaben String Aufgabe aus Datenbank ausgelesen

--- app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben.java
@@ -1,8 +1,11 @@
 package com.fabian.android.aufgabenliste;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.fabian.android.aufgabenliste.database.AufgabenlisteDatabase;
 
 public class Aufgaben extends AppCompatActivity
 {
@@ -14,10 +17,15 @@ public class Aufgaben extends AppCompatActivity
         super.onCreate (savedInstancesState);
         setContentView (R.layout.activity_aufgaben);
 
+        Intent intent = getIntent();
+        int i = Integer.valueOf (intent.getStringExtra(FragmentOne.ID_EXTRA));
+
         toolbar = (Toolbar) findViewById(R.id.toolbarVersion);
         setSupportActionBar (toolbar);
         getSupportActionBar().setTitle ("Aufgaben");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled (true);
+
+        String Aufgabe = AufgabenlisteDatabase.getAufgabe (i);
     }
 }
