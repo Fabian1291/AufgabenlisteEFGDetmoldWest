7d21d4930e08a6ece02266e98edad7aa37804d11
Fabian <fabiandollas@web.de>
2017-06-13 11:25:07 +0200

    in Klasse AufgabenlisteAdapter if-Abfrage geschrieben, ob das Datum gleich dem Datum in der Datenbank ist, und wenn es gleich ist wird die Uhrzeit anstelle vom Datum angezeigt

--- app/src/main/java/com/fabian/android/aufgabenliste/database/AufgabenlisteAdapter.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/database/AufgabenlisteAdapter.java
@@ -10,7 +10,10 @@ import android.widget.TextView;
 
 import com.fabian.android.aufgabenliste.R;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AufgabenlisteAdapter extends CursorAdapter
 {
@@ -18,6 +21,10 @@ public class AufgabenlisteAdapter extends CursorAdapter
     {
         super (context, cursor, 0);
     }
+
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date today = Calendar.getInstance().getTime();
+    String reportDate = df.format(today);
 
     @Override
     public View newView (final Context context, final Cursor cursor, final ViewGroup viewGroup)
@@ -29,7 +36,16 @@ public class AufgabenlisteAdapter extends CursorAdapter
     public void bindView (final View view, final Context context, final Cursor cursor)
     {
         ((TextView) view.findViewById (R.id.listitemAufgabe)).setText (cursor.getString (cursor.getColumnIndex (AufgabenlisteDatabase.AUFGABE_COLUMN)));
-        ((TextView) view.findViewById (R.id.listitemDatum)).setText (cursor.getString (cursor.getColumnIndex (AufgabenlisteDatabase.DATUM_COLUMN)));
+
+        if (cursor.getString (cursor.getColumnIndex (AufgabenlisteDatabase.DATUM_COLUMN)) == reportDate)
+        {
+            ((TextView) view.findViewById (R.id.listitemDatum)).setText (cursor.getString (cursor.getColumnIndex (AufgabenlisteDatabase.UHRZEIT_COLUMN)));
+        }
+        else
+        {
+            ((TextView) view.findViewById (R.id.listitemDatum)).setText (cursor.getString (cursor.getColumnIndex (AufgabenlisteDatabase.DATUM_COLUMN)));
+        }
+
         ((TextView) view.findViewById (R.id.listitemOrt)).setText (cursor.getString (cursor.getColumnIndex (AufgabenlisteDatabase.ORT_COLUMN)));
     }
 }
