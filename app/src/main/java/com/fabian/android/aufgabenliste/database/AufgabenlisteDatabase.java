e5c626db43d7eb1f2d86f2bb2300f5ec71eccda6
Fabian <fabiandollas@web.de>
2017-06-14 15:59:41 +0200

    in Klasse AufgabenlisteDatabase bei Cursor nach 0 ; entfernt und nach desc , entfernt

--- app/src/main/java/com/fabian/android/aufgabenliste/database/AufgabenlisteDatabase.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/database/AufgabenlisteDatabase.java
@@ -167,12 +167,12 @@ public class AufgabenlisteDatabase extends SQLiteOpenHelper
 
     public Cursor getAllAufgabenAsCursor ()
     {
-        return this.getReadableDatabase().rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ERLEDIGT_COLUMN + " = 0; " + "ORDER BY " + PRIORITAET_COLUMN + " DESC, " , null);
+        return this.getReadableDatabase().rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ERLEDIGT_COLUMN + " = 0 " + "ORDER BY " + PRIORITAET_COLUMN + " DESC" , null);
     }
 
     public Cursor getAllAufgabenErledigtAsCursor ()
     {
-        return this.getReadableDatabase().rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ERLEDIGT_COLUMN + " = 1; " + "ORDER BY " + PRIORITAET_COLUMN + " DESC, " , null);
+        return this.getReadableDatabase().rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ERLEDIGT_COLUMN + " = 1 " + "ORDER BY " + PRIORITAET_COLUMN + " DESC" , null);
     }
 
     public String getAufgabe (long Id) {
