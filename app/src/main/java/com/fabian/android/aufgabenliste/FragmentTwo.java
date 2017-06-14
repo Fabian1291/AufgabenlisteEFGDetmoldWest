0dc58827dbb69c0daa402daa85feb194e38e8150
Fabian <fabiandollas@web.de>
2017-06-14 15:23:00 +0200

    in Klasse FragmentTwo adapter und listview erstellt und Funktion onItemClick geschrieben

--- app/src/main/java/com/fabian/android/aufgabenliste/FragmentTwo.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/FragmentTwo.java
@@ -1,14 +1,22 @@
 package com.fabian.android.aufgabenliste;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.fabian.android.aufgabenliste.database.AufgabenlisteAdapter;
+import com.fabian.android.aufgabenliste.database.AufgabenlisteDatabase;
 
 public class FragmentTwo extends Fragment
 {
+    private ListView listView;
+    private AufgabenlisteAdapter adapter;
+
     public FragmentTwo ()
     {}
 
@@ -17,7 +25,23 @@ public class FragmentTwo extends Fragment
     {
         View view = inflater.inflate (R.layout.fragment_two, container, false);
 
-        ListView listView = (ListView) view.findViewById(R.id.listviewtwo);
+        this.adapter = new AufgabenlisteAdapter(getActivity(), AufgabenlisteDatabase.getInstance (getActivity()).getAllAufgabenErledigtAsCursor ());
+        this.listView.setAdapter (adapter);
+        this.listView.setOnItemClickListener (new AdapterView.OnItemClickListener ()
+        {
+            @Override
+            public void onItemClick (final AdapterView<?> adapterView, final View view, final int i, final long l)
+            {
+                Intent intent = new Intent(getActivity(), Aufgaben_Erledigt.class);
+
+                intent.putExtra ("id", l);
+
+                //zum Debuggen um die id anzuzeigen
+                //Toast.makeText(getActivity(), ""+l, Toast.LENGTH_LONG).show();
+
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
