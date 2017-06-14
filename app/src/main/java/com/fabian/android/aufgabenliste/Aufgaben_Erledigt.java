b91cec5589bc4c300ddcc812a5ee7c07b2f1f046
Fabian <fabiandollas@web.de>
2017-06-14 15:25:34 +0200

    in Klasse Aufgaben_Erledigt Funktionen update und getUsername entfernt

--- app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Erledigt.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Erledigt.java
@@ -72,49 +72,4 @@ public class Aufgaben_Erledigt extends AppCompatActivity
         TextView ersteller = (TextView) findViewById(R.id.textViewErsteller1);
         ersteller.setText(Ersteller);
     }
-
-    public void update (View view)
-    {
-        Intent intent = getIntent();
-        long i = intent.getExtras().getLong("id");
-
-        Intent intentErledigt = new Intent(this, MainActivity.class);
-
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-        Date today = Calendar.getInstance().getTime();
-        String reportDate = df.format(today);
-
-        DateFormat df1 = new SimpleDateFormat("HH:mm:ss");
-        Date today1 = Calendar.getInstance().getTime();
-        String reportTime = df1.format(today1);
-
-        AufgabenlisteDatabase database = AufgabenlisteDatabase.getInstance(Aufgaben.this);
-
-        database.updateAufgabe(i, getUsername(), reportDate, reportTime);
-
-        startActivity(intentErledigt);
-    }
-
-    public String getUsername() {
-        AccountManager manager = AccountManager.get(this);
-        Account[] accounts = manager.getAccountsByType("com.google");
-        List<String> possibleEmails = new LinkedList<String>();
-
-        for (Account account : accounts) {
-            // TODO: Check possibleEmail against an email regex or treat
-            // account.name as an email address only for certain account.type
-            // values.
-            possibleEmails.add(account.name);
-        }
-
-        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
-            String email = possibleEmails.get(0);
-            String[] parts = email.split("@");
-            if (parts.length > 0 && parts[0] != null)
-                return parts[0];
-            else
-                return null;
-        } else
-            return null;
-    }
 }
