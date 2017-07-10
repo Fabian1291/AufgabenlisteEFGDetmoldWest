a26422ffec7843e0dd73d861b2e39d599a57d584
Fabian <fabiandollas@web.de>
2017-07-11 01:00:35 +0200

    Klasse Register und layout activity_register erstellt

---
+++ app/src/main/java/com/fabian/android/aufgabenliste/Register.java
@@ -0,0 +1,13 @@
+package com.fabian.android.aufgabenliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
