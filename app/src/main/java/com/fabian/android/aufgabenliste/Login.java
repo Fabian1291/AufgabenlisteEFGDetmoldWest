4cba07aaf874c6ca2987c769572829e23ec7010d
Fabian <fabiandollas@web.de>
2017-07-16 19:07:09 +0200

    in Klasse Login Toolbar erstellt

--- app/src/main/java/com/fabian/android/aufgabenliste/Login.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Login.java
@@ -3,15 +3,24 @@ package com.fabian.android.aufgabenliste;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_login);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarLogin);
+        setSupportActionBar (toolbar);
+        getSupportActionBar().setTitle ("Log in");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled (false);
 
         Intent intent = new Intent (this, MainActivity.class);
 
