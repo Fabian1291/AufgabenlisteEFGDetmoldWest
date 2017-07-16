a6641f3ec0242d79fd8cff5222c082e84ff9e56e
Fabian <fabiandollas@web.de>
2017-07-16 18:56:48 +0200

    in Klasse Hinzufuegen Funktion zur Abfrage des Passworts geschrieben

--- app/src/main/java/com/fabian/android/aufgabenliste/Login.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Login.java
@@ -1,13 +1,31 @@
 package com.fabian.android.aufgabenliste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_login);
+
+        Intent intent = new Intent (this, MainActivity.class);
+
+        EditText editText = (EditText) findViewById (R.id.editTextPasswort);
+
+        String passwort = editText.getText ().toString ();
+
+        if (passwort.equals ("test"))
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText (this, "Falsches Passwort", Toast.LENGTH_LONG).show();
+        }
     }
 }
