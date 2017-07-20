f3ab656e4f7b99df1b79e9dd18f0a659114571c2
Fabian <fabiandollas@web.de>
2017-07-20 19:35:55 +0200

    in Klasse Aufgaben Funktion getUsername entfernt

--- app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben.java
@@ -94,27 +94,4 @@ public class Aufgaben extends AppCompatActivity
 
         startActivity(intentErledigt);
     }
-
-    public String getUsername() {
-        AccountManager manager = AccountManager.get(this);
-        Account[] accounts = manager.getAccountsByType("com.google");
-        List<String> possibleEmails = new LinkedList<String>();
-
-        for (Account account : accounts) {
-            // TODO: Check possibleEmail against an email regex or treat
-            // account.name as an email address only for certain account.type
-            // values.
-            possibleEmails.add(account.name);
-        }
-
-        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
-            String email = possibleEmails.get(0);
-            String[] parts = email.split("@");
-            if (parts.length > 0 && parts[0] != null)
-                return parts[0];
-            else
-                return null;
-        } else
-            return null;
-    }
 }
