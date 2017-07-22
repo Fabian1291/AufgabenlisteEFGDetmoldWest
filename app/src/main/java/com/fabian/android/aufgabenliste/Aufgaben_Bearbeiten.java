539523771edc0a0a2fb2bc354086feb3b1fbaa72
Fabian <fabiandollas@web.de>
2017-07-22 18:26:52 +0200

    in Klasse Aufgaben editText mit Text aus Datenbank gefüllt

--- app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Bearbeiten.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Bearbeiten.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.fabian.android.aufgabenliste.database.AufgabenlisteDatabase;
 
 public class Aufgaben_Bearbeiten extends AppCompatActivity
 {
@@ -22,5 +26,30 @@ public class Aufgaben_Bearbeiten extends AppCompatActivity
         getSupportActionBar().setTitle("Aufgabe Bearbeiten");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String Aufgabe = AufgabenlisteDatabase.getInstance(this).getAufgabe(i);
+
+        EditText aufgabe = (EditText) findViewById(R.id.editTextAufgabeBearbeiten);
+        aufgabe.setText(Aufgabe);
+
+        String Prioritaet = AufgabenlisteDatabase.getInstance(this).getPrioritaet(i);
+
+        TextView prioritaet = (TextView) findViewById(R.id.textViewPrioritaet1);
+        prioritaet.setText(Prioritaet);
+
+        String Ort = AufgabenlisteDatabase.getInstance(this).getOrt(i);
+
+        EditText ort = (EditText) findViewById(R.id.editTextOrtBearbeiten);
+        ort.setText(Ort);
+
+        String Beschreibung = AufgabenlisteDatabase.getInstance(this).getBeschreibung(i);
+
+        EditText beschreibung = (EditText) findViewById(R.id.editTextBeschreigungBearbeiten);
+        beschreibung.setText(Beschreibung);
+
+        String Ersteller = AufgabenlisteDatabase.getInstance(this).getErsteller(i);
+
+        EditText ersteller = (EditText) findViewById(R.id.editTextNameBearbeiten);
+        ersteller.setText(Ersteller);
     }
 }
