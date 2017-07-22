da91c0add70eea636c2dbe11c6a4530e88329eb0
Fabian <fabiandollas@web.de>
2017-07-22 18:11:39 +0200

    in Klasse Aufgaben_Erledigt Toolbar eingefügt

--- app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Bearbeiten.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Bearbeiten.java
@@ -1,4 +1,23 @@
 package com.fabian.android.aufgabenliste;
 
-public class Aufgaben_Bearbeiten {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Aufgaben_Bearbeiten extends AppCompatActivity
+{
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate (Bundle savedInstancesState) {
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.activity_aufgaben_bearbeiten);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarAufgaben);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Aufgabe Bearbeiten");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 }
