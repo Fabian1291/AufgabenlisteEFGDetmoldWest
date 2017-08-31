38b0f35e0ed90e612e6d3c587b92632751126f82
Fabian <fabiandollas@web.de>
2017-08-31 15:55:42 +0200

    in Klasse Aufgaben_Bearbeiten Spinnerabfrage hinzugefügt

--- app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Bearbeiten.java
+++ app/src/main/java/com/fabian/android/aufgabenliste/Aufgaben_Bearbeiten.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.fabian.android.aufgabenliste.database.AufgabenlisteDatabase;
@@ -20,6 +21,7 @@ public class Aufgaben_Bearbeiten extends AppCompatActivity
 
         Intent intent = getIntent();
         long i = intent.getExtras().getLong("id");
+        int spinneritem = 0;
 
         toolbar = (Toolbar) findViewById(R.id.toolbarAufgaben);
         setSupportActionBar(toolbar);
@@ -33,6 +35,21 @@ public class Aufgaben_Bearbeiten extends AppCompatActivity
         aufgabe.setText(Aufgabe);
 
         String Prioritaet = AufgabenlisteDatabase.getInstance(this).getPrioritaet(i);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        if (Prioritaet.equals ("0-sehr niedrig"))
+            spinneritem = 0;
+        else if (Prioritaet.equals ("1-niedrig"))
+            spinneritem = 1;
+        else if (Prioritaet.equals ("2-normal"))
+            spinneritem = 2;
+        else if (Prioritaet.equals ("3-hoch"))
+            spinneritem = 3;
+        else if (Prioritaet.equals ("4-sehr hoch"))
+            spinneritem = 4;
+        else if (Prioritaet.equals ("5-dringend"))
+            spinneritem = 5;
+
+        spinner.setSelection(spinneritem);
 
         TextView prioritaet = (TextView) findViewById(R.id.textViewPrioritaet1);
         prioritaet.setText(Prioritaet);
